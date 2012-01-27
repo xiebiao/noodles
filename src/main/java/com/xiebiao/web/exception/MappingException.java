bf354ca17ec8ad24b219689a97557c971448b2e3
xiaog <joyrap@gmail.com>
2012-01-27 22:32:49 +0800

    add MappingException

---
+++ src/main/java/com/xiebiao/web/exception/MappingException.java
@@ -0,0 +1,10 @@
+package com.xiebiao.web.exception;
+
+public class MappingException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public MappingException(String msg) {
+		super(msg);
+	}
+}
