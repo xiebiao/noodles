e85a2122a5b01a9b6ef38b519f2614cd11c7a9f3
xiaog <joyrap@gmail.com>
2012-01-29 18:19:55 +0800

    添加freemarker配置文件

--- src/main/java/com/xiebiao/web/template/FreemarkerTemplate.java
+++ src/main/java/com/xiebiao/web/template/FreemarkerTemplate.java
@@ -1,13 +1,13 @@
 package com.xiebiao.web.template;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.Writer;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.slf4j.LoggerFactory;
 
 import com.xiebiao.web.RequestContext;
 import com.xiebiao.web.exception.RenderException;
@@ -15,34 +15,44 @@ import com.xiebiao.web.exception.RenderException;
 import freemarker.template.Configuration;
 
 public class FreemarkerTemplate implements Template {
-	private final org.slf4j.Logger LOG = LoggerFactory.getLogger(this
-			.getClass());
-	private String templateFile;
-	private Map<String, Object> data;
-	private final static Configuration cfg = new Configuration();
-	
-	public FreemarkerTemplate(String templateFile, Map<String, Object> data) {
-		this.templateFile = templateFile;
-		this.data = data;
+	private String _templateFile;
+	private final static String _configFile = "freemarker.properties";
+	private Map<String, Object> _data;
+	private final static Configuration _cfg = new Configuration();
 
+	public FreemarkerTemplate(String templateFile, Map<String, Object> data) {
+		this._templateFile = templateFile;
+		this._data = data;
+		Properties p = new Properties();
+		File f = new File(RequestContext.getCurrent().getServletContext()
+				.getRealPath("")
+				+ "/WEB-INF/" + _configFile);
+		try {
+			p.load(new FileInputStream(f));
+			_cfg.setSettings(p);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void render(HttpServletRequest request, HttpServletResponse response)
 			throws RenderException {
+
 		try {
 			String templatesDirectory = RequestContext.getCurrent()
 					.getServletContext().getRealPath("")
 					+ "/WEB-INF/templates/";
-			cfg.setDirectoryForTemplateLoading(new File(templatesDirectory));
-			freemarker.template.Template template = cfg
-					.getTemplate(this.templateFile);
+			_cfg.setDirectoryForTemplateLoading(new File(templatesDirectory));
+			freemarker.template.Template template = _cfg
+					.getTemplate(this._templateFile);
 			response.setContentType("text/html; charset="
 					+ RequestContext.ENCODING);
 			Writer out = response.getWriter();
-			template.process(data, out);
+			template.process(_data, out);
 		} catch (Exception e) {
 			e.printStackTrace();
-			throw new RenderException(e.getMessage());
+			throw new RenderException("");
 		}
+
 	}
 }
